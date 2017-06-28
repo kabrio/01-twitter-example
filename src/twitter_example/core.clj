d2a83d37d56c3d7482a03a85d1304be18a499853
Jo Bo <jbohatsch@gmail.com>
2017-06-28 10:54:11 +0200

    scramble words and sentences

--- src/twitter_example/core.clj
+++ src/twitter_example/core.clj
@@ -16,18 +16,19 @@
 ; We define the "templates" - these strings are the skeleton of the tweet
 ; We will later replace every occurence of ___ with a string that we chose
 ; randomly from the list "blanks".
-(def templates ["I hear he eats ___"
-                "The police arrested ___ in connection with the robbery."
-                "I don't know if ___ will visit us next Sunday."
-                "She sent a card to ___."
-                "Doing that sort of thing makes you look like ___."])
+(def templates ["I found a nice ___"
+                "The ambassador greets ___ & everyone else travelling"
+                "I don't know if ___ will visit us next Millenial"
+                "I sent a message to ___"
+                "Doing that sort of thing makes you look like ___"])
 
 ; Next we define the "blanks"
-(def blanks ["a Haunted House"
-             "Rich Hickey"
-             "a purple dinosaur"
-             "Junk Mail"
-             "Pirates"])
+(def blanks ["beautiful exoplanet"
+             "Charles Darwin"
+             "rainbow butterfly"
+             "Sudoku"
+             "Belters"
+             "all beings"])
 
 ; generate-sentence returns a random sentence, built by choosing one template
 ; string at random and filling in the blank space (___) with a randomly chosen
@@ -37,6 +38,37 @@
         blank (rand-nth blanks)]
       (s/replace template "___" blank)))
 
+
+(defn alter-word [word]
+  (if (> (count word) 1)
+    (def wordStart (subs word 0 2))
+    (def wordStart word))
+  (if (> (count word) 1)
+    (def wordEnd (subs word (- (count word) 2) (count word)))
+    (def wordEnd ""))
+  (def wordEnd2 (s/reverse wordEnd))
+  (def newWord (str wordStart wordEnd2))
+  newWord)
+
+(defn generate-scramble []
+  (let [template (rand-nth templates)
+        blank (rand-nth blanks)]
+      (def myWords (s/split (s/replace template "___" blank) #" ")))
+  (def newSen "")
+  (def original "")
+  (doseq [word myWords] (def newSen (str newSen (alter-word word) " ")) (def original (str original word " ")))
+  ;(pr (str (subs original 0 (- (count original) 1)) "."))
+  ;(newline)
+  (def finalSentence (str (subs newSen 0 (- (count newSen) 1)) "..."))
+  finalSentence)
+
+(defn scramble [sentence]
+  (def myWords (s/split sentence #" "))
+  (def newSen "")
+  (doseq [word myWords] (def newSen (str newSen (alter-word word) " ")))
+  (def finalSentence (str (subs newSen 0 (- (count newSen) 1)) "..."))
+  finalSentence)
+
 ; We retrieve the twitter credentials from the profiles.clj file here.
 ; In profiles.clj we defined the "env(ironment)" which we use here
 ; to get the secret passwords we need.
@@ -45,6 +77,7 @@
                                                          (env :app-consumer-secret)
                                                          (env :user-access-token)
                                                          (env :user-access-secret)))
+
 
 ; Tweets are limited to 140 characters. We might randomly generate a sentence
 ; with more than 140 characters, which would be rejected by Twitter.
@@ -60,11 +93,13 @@
 (defn tweet-sentence []
   (tweet (generate-sentence)))
 
+(defn tweet-scramble []
+  (tweet (generate-scramble)))
+
 
 (def my-pool (overtone/mk-pool))
 
 (defn -main [& args]
   ;; every 2 hours
   (println "Started up")
-  (println (tweet-sentence))
-  (overtone/every (* 1000 60 60 2) #(println (tweet-sentence)) my-pool))
+  (overtone/every (* 1000 60 60 8) #(println (tweet-scramble)) my-pool))
